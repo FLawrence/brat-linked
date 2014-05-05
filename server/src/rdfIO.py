d36aa07b68ce4b573665eba683d275142f39789c
k.faith.lawrence@googlemail.com <k.faith.lawrence@googlemail.com@c87d138e-f7b0-0930-913d-0ac75b28e58d>
2014-05-05 17:43:07 +0000

    RDF corrections, added more classes and relationships and removed some of the debug

--- server/src/rdfIO.py
+++ server/src/rdfIO.py
@@ -8,12 +8,12 @@ namespace = 'http://contextus.net/data/RRH/'
 
 namespaces = {'ome':'http://contextus.net/ontology/ontomedia/core/expression#', 'omt':'http://contextus.net/ontology/ontomedia/ext/common/trait#', 'omb':'http://contextus.net/ontology/ontomedia/ext/common/being#', 'omeg':'http://contextus.net/ontology/ontomedia/ext/events/gain#', 'omel':'http://contextus.net/ontology/ontomedia/ext/events/loss#', 'omet':'http://contextus.net/ontology/ontomedia/ext/events/trans#', 'omes':'http://contextus.net/ontology/ontomedia/ext/events/social#', 'omea':'http://contextus.net/ontology/ontomedia/ext/events/action#', 'omj':'http://contextus.net/ontology/ontomedia/ext/events/travel#', 'eprop':'http://contextus.net/ontology/ontomedia/ext/events/eventprop#', 'omf':'http://contextus.net/ontology/ontomedia/ext/fiction/fic#'}
     
-category_map = {'Entity':'ome', 'Being':'ome', 'Character':'ome', 'Item':'ome', 'Abstract-Item':'ome', 'Group':'omb', 'Community':'omb', 'Household':'omb', 'Bonded-Group':'omb', 'Bonded-Pair':'omb', 'Organisation':'omb', 'Company':'omb', 'Government':'omb', 'Context':'ome', 'Physical-Item':'ome', 'Space':'ome','Event':'ome', 'Gain':'ome', 'Creation':'omeg', 'Loss':'ome', 'Destruction':'omel', 'Betrayal':'omel', 'Transformation':'ome', 'Transference':'omet', 'Division':'omet', 'Merge':'omet', 'Degradation':'omet', 'Social':'ome', 'Conversational':'omes', 'Flirtation':'omes', 'Proposition':'omes', 'Political':'omes', 'Academic':'omes', 'Legal':'omes', 'Theological':'omes', 'Philosophical':'omes', 'Action':'ome', 'Violence':'omea', 'Sex':'omea', 'Festivity':'omea', 'Ingestion':'omea', 'Celestial':'omea', 'Environmental':'omea', 'Travel':'omj', 'Describes':'omf', 'Implied':'omf', 'References-Concept':'omf', 'In-Passing':'omf', 'Vague-Description':'omf', 'Detailed-Description':'omf', 'Extremely-Detailed-Description':'omf', 'Fade-To-Black':'omf', 'Extremely-Detailed-Description':'omf', 'Spoiler':'omf', 'Key':'omf', 'Main':'omf', 'Fact':'omf', 'Nitpick':'omf', 'Nitpick':'omf', 'Consent-Given':'eprop', 'Consent-Implied':'eprop', 'Consent-Not-Given':'eprop', 'Consent-Unclear':'eprop', 'Strongly-Positive':'omes','Positive':'omes','Neutral':'omes','Negative':'omes','Strongly-Negative':'omes',}
+category_map = {'Entity':'ome', 'Being':'ome', 'Character':'ome', 'Item':'ome', 'Abstract-Item':'ome', 'Group':'omb', 'Community':'omb', 'Household':'omb', 'Bonded-Group':'omb', 'Bonded-Pair':'omb', 'Organisation':'omb', 'Company':'omb', 'Government':'omb', 'Context':'ome', 'Physical-Item':'ome', 'Space':'ome','Event':'ome', 'Gain':'ome', 'Creation':'omeg', 'Loss':'ome', 'Destruction':'omel', 'Betrayal':'omel', 'Transformation':'ome', 'Transference':'omet', 'Division':'omet', 'Merge':'omet', 'Degradation':'omet', 'Social':'ome', 'Conversational':'omes', 'Flirtation':'omes', 'Proposition':'omes', 'Political':'omes', 'Academic':'omes', 'Legal':'omes', 'Theological':'omes', 'Philosophical':'omes', 'Action':'ome', 'Violence':'omea', 'Sex':'omea', 'Festivity':'omea', 'Ingestion':'omea', 'Celestial':'omea', 'Environmental':'omea', 'Travel':'omj', 'Describes':'omf', 'Implied':'omf', 'References-Concept':'omf', 'In-Passing':'omf', 'Vague-Description':'omf', 'Detailed-Description':'omf', 'Extremely-Detailed-Description':'omf', 'Fade-To-Black':'omf', 'Extremely-Detailed-Description':'omf', 'Spoiler':'omf', 'Key':'omf', 'Main':'omf', 'Fact':'omf', 'Nitpick':'omf', 'Nitpick':'omf', 'Consent-Given':'eprop', 'Consent-Implied':'eprop', 'Consent-Not-Given':'eprop', 'Consent-Unclear':'eprop', 'Strongly-Positive':'omes','Positive':'omes','Neutral':'omes','Negative':'omes','Strongly-Negative':'omes'}
 
     
 relationship_map = {'is-linked-to':'ome', 'is':'ome', 'is-shadow-of':'ome', 'contains':'ome', 'contained-by':'ome', 'has-subject-entity':'ome', 'has-object-entity':'ome', 'has-subject':'omes', 'to':'ome', 'from':'ome'}
     
-extended_rdf_map = {'bond-with':'omt:has-trait [\n\ta omt:link ; [\n\t\ta omb:Bond;\n\t\tomb:has-bond {1}].\n\t].\n', 'family-of':'omt:has-trait [\n\ta omt:link ; [\n\t\ta omb:Family;\n\t\tomb:is-relation-of {1}].\n\t].\n'}
+extended_rdf_map = {'bond-with':'omt:has-trait [\n\ta omt:link ;\n\t\tomb:has-bond [\n\t\t\ta omb:Bond;\n\t\tome:is-linked-to {1}].\n\t\t].\n', 'family-of':'omt:has-trait [\n\ta omt:link ;\n\t\tomb:has-bond [\n\t\t\ta omb:Family;\n\t\tomb:is-relation-of {1}].\n\t\t].\n', 'friend-of':'omt:has-trait [\n\ta omt:link ;\n\t\tomb:has-bond [\n\t\t\ta omb:Friendship;\n\t\tome:is-linked-to {1}].\n\t\t].\n', 'enemy-of':'omt:has-trait [\n\ta omt:link ;\n\t\tomb:has-bond [\n\t\t\ta omb:Enmity;\n\t\tomb:is-linked-to {1}].\n\t\t].\n'}
 
 def convert_to_rdf(fpath):
     
@@ -48,12 +48,12 @@ def convert_to_rdf(fpath):
                 rdf += "\trdf:label " + chunks[0] + " .\n\n"
                 
             elif line[0] == 'N':        
-                rdf += "{N}<" + namespace + chunks[4] + "> owl:sameAs <" + namespace + chunks[3] + ">;\n"
+                rdf += "<" + namespace + chunks[4] + "> owl:sameAs <" + namespace + chunks[3] + ">;\n"
                 rdf += "\trdf:seeAlso " + namespace + chunks[0] + " .\n\n"
            
             elif line[0] == 'R':
                 
-                rdf += "{R}<" + namespace + chunks[2].split(":")[1] + "> "
+                rdf += "<" + namespace + chunks[2].split(":")[1] + "> "
                     
                 if lookup(chunks[1]) != False:
                     rdf += lookup(chunks[1]) + " " + "<" + namespace + chunks[3].split(":")[1] + ">;\n" 
@@ -63,18 +63,24 @@ def convert_to_rdf(fpath):
                 rdf += "\trdf:label '" + chunks[0] + "' .\n\n"
             
             elif line[0] == 'T':
-                rdf += "{T}<" + namespace + chunks[0] + ">\n\ta <"
+                rdf += "<" + namespace + chunks[0] + ">\n\ta <"
                 if lookup(chunks[1]) != False:
                     rdf += lookup(chunks[1])
                 rdf += "> .\n\n"
             
             elif line[0] == 'A':
-                rdf += "{A}<" + namespace + chunks[2] + ">\n\ta <" + lookup(chunks[3]) + ">;\n"    
+                rdf += "<" + namespace + chunks[2] + ">\n\ta <" + lookup(chunks[3]) + ">;\n"    
                 rdf += "\trdf:label '" + chunks[0] + "' .\n\n"     
            
     return rdf
 
 def lookup(annotation):
+
+    conversion = {'Given':'Consent-Given', 'Implied':'Consent-Implied', 'Not-Given':'Consent-Not-Given', 'Unclear':'Consent-Unclear'}
+    
+    if annontation in conversion
+        annotation = conversion[annotation]
+
     if annotation in namespaces:
         return namespaces[annotation]
     elif annotation in category_map:
