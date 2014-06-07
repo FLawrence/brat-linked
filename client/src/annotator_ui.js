44c5cf67a935c9b1fa275cff890b703cd05f4087
KludgeKML@gmail.com <KludgeKML@gmail.com@c87d138e-f7b0-0930-913d-0ac75b28e58d>
2014-06-07 17:05:52 +0000

    Fix missing quotes

--- client/src/annotator_ui.js
+++ client/src/annotator_ui.js
@@ -1286,7 +1286,7 @@ var AnnotatorUI = (function($, window, undefined) {
         dispatcher.post('hideForm');
         $('#norm_search_button').val('Search ' + $('#span_norm_db').val());
         setNormSearchSubmit(false);
-        $('#norm_create_button').button(disable);
+        $('#norm_create_button').button('disable');
         dispatcher.post('showForm', [normSearchDialog]);
         $('#norm_search_query').focus().select();
       }
