54dbeacc72002cf023bc32f584db5db50b263e6b
KludgeKML@gmail.com <KludgeKML@gmail.com@c87d138e-f7b0-0930-913d-0ac75b28e58d>
2014-06-08 13:15:44 +0000

    Add debug to try to sort the string issue.

--- server/src/norm.py
+++ server/src/norm.py
@@ -89,6 +89,7 @@ def norm_create_name(database, name, collection=None):
         Messager.warning(str(e))
 
     strings = normdb.get_all_entity_strings(dbpath)
+    Messager.info('Strings = [' + str.join(strings,',') + ']')
 
     simstringdb.ssdb_build(strings, database)
      
