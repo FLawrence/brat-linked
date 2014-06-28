3567ad276a71984cf0fc138cdf019cf1c2d63efc
k.faith.lawrence@googlemail.com <k.faith.lawrence@googlemail.com@c87d138e-f7b0-0930-913d-0ac75b28e58d>
2014-06-28 21:09:09 +0000

    'and' not '&&' in conditionals

--- server/src/annotator.py
+++ server/src/annotator.py
@@ -427,7 +427,7 @@ def create_span(collection, document, offsets, type, attributes=None,
     # end) pairs; convert once at this interface
     offsets = _json_offsets_to_list(offsets)
     
-    if(linkedNorm != None && normalizations != None):    
+    if(linkedNorm != None and normalizations != None):    
         Messager.info("Linked span id: "+ linkedNorm + ", ID: " + normalizations['normId'])
     elif(normalizations != None):
         Messager.info("Linked span id: , ID: " +  normalizations['normId'])
