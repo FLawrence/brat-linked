20ba817e4bce382c9a0ff9aa278560abc3241c51
k.faith.lawrence@googlemail.com <k.faith.lawrence@googlemail.com@c87d138e-f7b0-0930-913d-0ac75b28e58d>
2014-06-29 10:17:27 +0000

    expanding feedback message

--- server/src/annotator.py
+++ server/src/annotator.py
@@ -433,7 +433,7 @@ def create_span(collection, document, offsets, type, attributes=None,
     
     update_info = norm_update_link(database, local_uid, linkedNorm)
     
-    Messager.info('Local ID: ' + update_info['local'] + ', Global ID: ' +  update_info['global'] + ', Response: ' + update_info['repsonse'])
+    Messager.info('Local ID: ' + update_info['local'] + ', Global ID: ' +  update_info['global'])
 
     return _create_span(collection, document, offsets, type, attributes,
                         normalizations, id, comment)
