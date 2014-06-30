2aea7c9351732e5c271931adae85a48a8c1f6f66
KludgeKML@gmail.com <KludgeKML@gmail.com@c87d138e-f7b0-0930-913d-0ac75b28e58d>
2014-06-30 22:22:00 +0000

    Fixed debug

--- server/src/dispatch.py
+++ server/src/dispatch.py
@@ -339,9 +339,17 @@ def dispatch(http_args, client_ip, client_hostname):
                        'FINISH', action, action_args)
 
     if action in ANNOTATION_SAVE_RDF:
-        Messager.info("Saving command from " + action + ", " +
-            (http_args['collection'] == None ? "None": http_args['collection']) + ", " +
-            (http_args['document'] == None ? "None": http_args['document']))
+        if http_args['collection'] == None:
+            hc = "None"
+        else:
+            hc = http_args['collection']
+
+        if http_args['document'] == None:
+            hd = "None"
+        else:
+            hd = http_args['document']
+
+        Messager.info("Saving command from " + action + ", " + hc + ", " + hd)
         create_rdf_file(http_args['collection'],
                         http_args['document'])
 
