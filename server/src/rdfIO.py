b0404513511a0af5afd115d8a8bd30f859caa3cf
KludgeKML@gmail.com <KludgeKML@gmail.com@c87d138e-f7b0-0930-913d-0ac75b28e58d>
2014-06-30 21:37:03 +0000

    Fix import

--- server/src/rdfIO.py
+++ server/src/rdfIO.py
@@ -11,7 +11,7 @@ from config import DATA_DIR
 from document import real_directory
 from message import Messager
 from session import get_session
-from normdb import get_norm_type_by_id, data_by_id
+from normdb import get_norm_type_by_id, data_by_id, get_linked_global_entity
 
 # Constants
 RDF_FILE_SUFFIX = 'rdf'
