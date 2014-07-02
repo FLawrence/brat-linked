6509a55d339ebb0d5753851dda762b2829d7e9b3
k.faith.lawrence@googlemail.com <k.faith.lawrence@googlemail.com@c87d138e-f7b0-0930-913d-0ac75b28e58d>
2014-07-02 22:52:12 +0000

    added debug

--- server/src/rdfIO.py
+++ server/src/rdfIO.py
@@ -179,6 +179,9 @@ def get_rdf_parts(fpath, document):
                                 parts['data'] += '\trdfs:label "' + data_tuple[1] + '";\n'
                             elif data_tuple[0] == 'Category':
                                 parts['data'] += '\ta ' + lookup(data_tuple[1], namespace_info) + ' .\n\n'
+                                
+    parts += " ".join(entity_data) 
+                                
     return parts
 
 
