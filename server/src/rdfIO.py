36cb1833c90390aa4f4852c700e8b855c03c949f
k.faith.lawrence@googlemail.com <k.faith.lawrence@googlemail.com@c87d138e-f7b0-0930-913d-0ac75b28e58d>
2014-07-05 06:19:46 +0000

    remove 'a' predicate for trait triples

--- server/src/rdfIO.py
+++ server/src/rdfIO.py
@@ -166,14 +166,14 @@ def get_rdf_parts(fpath, document):
                 get_lookup = lookup(chunks[1], namespace_info)
                 
                 if (get_lookup == chunks[1]):
-                    get_lookup = lookup(chunks[3], namespace_info) + ';'
+                    get_lookup = 'a ' + lookup(chunks[3], namespace_info) + ';'
                 elif (get_lookup == False):
                     get_lookup = get_long_rdf(chunks[1], namespace_info, chunks[3:]) 
                 else:
-                    get_lookup += ';'
+                    get_lookup = 'a ' + get_lookup + ';'
                 
                 
-                parts['data'] += "<" + namespace + chunks[2] + ">\n\ta " + get_lookup + "\n"
+                parts['data'] += "<" + namespace + chunks[2] + ">\n\t " + get_lookup + "\n"
                 parts['data'] += '\trdfs:label "' + chunks[0] + '" .\n\n'
 
         if len(entity_data) > 0:
